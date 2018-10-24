33c1e37ed145c35ddfabc60966e157876476e565
Dan Frohlich <Daniel.Frohlich@kroger.com>
2018-10-24 07:20:59 -0400

    slides

--- remark/index.html.gen.sh
+++ remark/index.html.gen.sh
@@ -11,12 +11,18 @@ cat << END
       img{
         object-fit: contain;
         width: 100%;
-        border: thin solid red;
+        border: thin solid cornflowerblue;
       }
     </style>
   </head>
   <body>
      <textarea id="source">
+layout: true
+background-image: url(resources/watercolor.jpeg)
+*https://github.com/dan-frohlich/learn-canvas-in-8-hours*
+
+---
+
 ${MARKDOWN}
     </textarea>
      <script src="remark/remark-latest.min.js">
